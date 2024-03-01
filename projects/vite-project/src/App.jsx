f1eb9b093c9ebb254872be89870f52de93929fe6
Erdwin Nandayapa Cordero <nandayapaerdwin956@gmail.com>
2024-03-01 17:06:11 -0600

    Refactor TwitterFollowCard component and update usage in App.jsx

--- projects/vite-project/src/App.jsx
+++ projects/vite-project/src/App.jsx
@@ -1,37 +1,25 @@
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
 export function App() {
-  const formatUserName = (userName) => `@${userName}`; //los props puedo pasar tambien funciones
+  // const formatUserName = (userName) => `@${userName}`; //los props puedo pasar tambien funciones
+
   return (
     <>
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        isFollowing
-        userName="erdwin"
-        name="Erdwin Nandayapa"
-      />
+      <TwitterFollowCard isFollowing userName="erdwin">
+        Erdwin Nandayapa
+      </TwitterFollowCard>
       ;
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        isFollowing
-        userName="horacio"
-        name="Sensei Horacio"
-      />
+      <TwitterFollowCard isFollowing userName="horacio">
+        Sensei Horacio
+      </TwitterFollowCard>
       ;
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        isFollowing
-        userName="sara"
-        name="QA sara"
-      />
+      <TwitterFollowCard isFollowing userName="sara">
+        QA sara
+      </TwitterFollowCard>
       ;
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        isFollowing
-        userName="Mike"
-        name="Mike Nieva"
-      />
-      ;
+      <TwitterFollowCard isFollowing userName="Mike">
+        Mike Nieva
+      </TwitterFollowCard>
     </>
   );
 }
--- projects/vite-project/src/TwitterFollowCard.jsx
+++ projects/vite-project/src/TwitterFollowCard.jsx
@@ -1,27 +1,36 @@
-export function TwitterFollowCard({
-  formatUserName,
-  userName,
-  name,
-  isFollowing,
-}) {
-  console.log(isFollowing);
+import { useState } from "react";
+
+export function TwitterFollowCard({ children, userName, initialIsFollowing }) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing
+    ? "tw-followCard-button is-following"
+    : "tw-followCard-button";
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
+
   return (
     <article className="tw-followCard">
       <header className="tw-followCard-header">
         <img
           className="tw-followCard-avatar"
+          alt="El avatar de midudev"
           src={`https://unavatar.io/${userName}`}
-          alt="erdwin"
         />
         <div className="tw-followCard-info">
-          <strong>{name}</strong>
-          <span className="tw-followCard-infoUserName">
-            {formatUserName(userName)}
-          </span>
+          <strong>{children}</strong>
+          <span className="tw-followCard-infoUserName">@{userName}</span>
         </div>
       </header>
+
       <aside>
-        <button className="tw-followCard-button">seguir</button>
+        <button className={buttonClassName} onClick={handleClick}>
+          <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+        </button>
       </aside>
     </article>
   );
