4c3bf74971b0e7acab540e78f239656a980df787
Erdwin Nandayapa Cordero <nandayapaerdwin956@gmail.com>
2024-03-10 15:42:27 -0600

    Update index.html and App.jsx

--- projects/vite-project/src/App.jsx
+++ projects/vite-project/src/App.jsx
@@ -3,23 +3,42 @@ import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
 export function App() {
   // const formatUserName = (userName) => `@${userName}`; //los props puedo pasar tambien funciones
 
+  const data = [
+    {
+      userName: "erdwin",
+      name: "Erdwin Nandayapa",
+      isFollowing: true,
+    },
+    {
+      userName: "horacio",
+      name: "Sensei Horacio",
+      isFollowing: false,
+    },
+    {
+      userName: "sara",
+      name: "Sara Halfonsin",
+      isFollowing: true,
+    },
+    {
+      userName: "Mike",
+      name: "Mike Nieva",
+      isFollowing: false,
+    },
+  ];
+
   return (
     <>
-      <TwitterFollowCard isFollowing userName="erdwin">
-        Erdwin Nandayapa
-      </TwitterFollowCard>
-      ;
-      <TwitterFollowCard isFollowing userName="horacio">
-        Sensei Horacio
-      </TwitterFollowCard>
-      ;
-      <TwitterFollowCard isFollowing userName="sara">
-        QA sara
-      </TwitterFollowCard>
-      ;
-      <TwitterFollowCard isFollowing userName="Mike">
-        Mike Nieva
-      </TwitterFollowCard>
+      <section className="App">
+        {data.map((user) => (
+          <TwitterFollowCard
+            key={user.userName}
+            userName={user.userName}
+            initialIsFollowing={user.isFollowing}
+          >
+            {user.name}
+          </TwitterFollowCard>
+        ))}
+      </section>
     </>
   );
 }
